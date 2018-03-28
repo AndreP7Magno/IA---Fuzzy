9f4a8fbfbe6f3eb0d6a03122fd66279813e5ba01
Andre <andremagno14@gmail.com>
2018-03-28 20:22:14 -0300

    subindo arquivo da aula. Menu principal implementado

--- IA - Fuzzy/Program.cs
+++ IA - Fuzzy/Program.cs
@@ -10,7 +10,12 @@ namespace IA___Fuzzy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Coemçou");
+            Console.WriteLine("Aguarde enquanto estamos recolhendo as informações do usuário...");
+
+            //LER TXT USUÁRIO - GUARDAR NUMA LISTA DE USUÁRIOS
+
+            System.Threading.Thread.Sleep(5000);
+            Console.Clear();
 
             MontaMenu();
 
@@ -18,7 +23,55 @@ namespace IA___Fuzzy
 
         public static void MontaMenu()
         {
+            Console.WriteLine("╔════════════════════════════════════════════════╗");
+            Console.WriteLine("║ \tPor favor escolha a disciplina desejada. ║");
+            Console.WriteLine("║                                                ║");
+            Console.WriteLine("║ 1. = PROGRAMAÇÃO                               ║");
+            Console.WriteLine("║ 2. = EXCLUIR CADASTRO                          ║");
+            Console.WriteLine("║ 3. = EDITAR CADASTRO                           ║");
+            Console.WriteLine("║ 4. = VOLTAR AO MENU                            ║");
+            Console.WriteLine("║ 5. = VOLTAR AO MENU                            ║");
+            Console.WriteLine("╚════════════════════════════════════════════════╝");
+            Console.WriteLine("");
+            Console.Write("SUA OPÇÃO: ");
+            var opcao = Console.ReadLine();
 
+            MontaLogica(opcao);
+        }
+
+        public static void MontaLogica(string opcao)
+        {
+            Console.Clear();
+            Console.WriteLine("Verificando opção fornecida...");
+
+            switch (opcao)
+            {
+                case "1":
+                    InformaUsuario();
+                    break;
+                case "2":
+                    InformaUsuario();
+                    break;
+                case "3":
+                    InformaUsuario();
+                    break;
+                case "4":
+                    InformaUsuario();
+                    break;
+                case "5":
+                    InformaUsuario();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Opção Inválida.");
+
+                    break;
+            }
+        }
+
+        public static void InformaUsuario(){
+            Console.Clear();
+            Console.WriteLine("Aguarde enquanto estamos montando a melhor dupla possível para seu trabalho...");
         }
     }
 }
